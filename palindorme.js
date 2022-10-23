044bbd8ac8a5d707f9f90987eb36abe00d0492d4
darkkira <hemant2307sb@gmail.com>
2022-10-23 16:27:35 +0530

    to check wheather the string is palindrome or not

---
+++ palindorme.js
@@ -0,0 +1,16 @@
+// check wheather the string is palindrome or not 
+function checkPalindrome(N, str) {
+    
+    let newString=''
+    for(let i=N-1;i>=0;i--){
+        newString=newString+str[i]
+    }
+    
+    if(str==newString){
+        
+    }
+    
+    }
+
+}
+checkPalindrome(3,"mom")
